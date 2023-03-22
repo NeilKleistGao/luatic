9fa60ee3458df01c06a0589ea728fd105f0abbde
NeilKleistGao <neilkleist@gmail.com>
2023-03-22 19:36:35 +0800

    Refactor generator

--- generate_instructions.sh
+++ generate_instructions.sh
@@ -1,3 +1,11 @@
-ocamlc -o bin/instruction_generator generator/instructions.ml
+#!/usr/bin/env bash
+
+cd generator
+
+ocamlopt -c cpplib.ml
+ocamlopt -c instructions.ml
+ocamlopt -o ../bin/instruction_generator cpplib.cmx instructions.cmx 
+
+cd ..
 ./bin/instruction_generator
 ./clang_format.sh
