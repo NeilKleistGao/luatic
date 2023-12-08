28727a3667c1dd5d054df193d1c734b1557f79c7
NeilKleistGao <neilkleist@gmail.com>
2023-12-08 15:37:32 +0800

    Finish tokenizer

--- src/luatic/compiler.rs
+++ src/luatic/compiler.rs
@@ -1,4 +1,6 @@
 use super::tokenizer::{tokenize};
+use super::exceptions::Exception;
+
 
 pub struct CompileOption {
   filename: String,
@@ -11,11 +13,21 @@ impl CompileOption {
   }
 }
 
+fn format_errors(errors: &Vec<Exception>) -> String {
+  "".to_string() // TODO
+}
+
 pub fn compile(option: CompileOption) -> Result<(), String> {
   match std::fs::read_to_string(&option.filename) {
     Err(why) => Err(why.to_string()),
     Ok(code) => {
-      let tokens = tokenize(code);
+      let tokens = match tokenize(code).get_result() {
+        Ok(res) => res,
+        Err(msgs) => {
+          let err = format_errors(msgs);
+          return Err(err);
+        }
+      };
       Ok(())
     }
   }
--- src/luatic/tokenizer.rs
+++ src/luatic/tokenizer.rs
@@ -1,5 +1,5 @@
 use super::tokens::{Token, TokenPack, Literal};
-use super::exceptions::{Position, Location, Exception};
+use super::exceptions::{Position, Location};
 
 static mut LOC_ROW: usize = 1;
 static mut LOC_COL: usize = 1;
@@ -8,6 +8,12 @@ fn new_line() {
   unsafe {
     LOC_ROW = LOC_ROW + 1;
     LOC_COL = 1;
+  }
+}
+
+fn step() {
+  unsafe {
+    LOC_COL = LOC_COL + 1;
   }
 }
 
@@ -24,9 +30,27 @@ fn get_position() -> Position {
   }
 }
 
-fn swallow_line(cur: &mut std::str::Chars<'_>) {
+fn next(cur: &mut std::str::Chars<'_>) -> Option<char> {
   loop {
     match cur.next() {
+      res @ Some(c) => {
+        if c == '\n' {
+          new_line();
+        }
+        else {
+          step();
+        }
+
+        return res
+      }
+      res => return res
+    }
+  }
+}
+
+fn swallow_line(cur: &mut std::str::Chars<'_>) {
+  loop {
+    match next(cur) {
       Some('\n') => {
         new_line();
         break
@@ -41,9 +65,9 @@ fn parse_string(cur: &mut std::str::Chars<'_>) -> Result<Token, (String, Locatio
   let mut s = String::new();
   let start_pos = get_position();
   loop {
-    match cur.next() {
+    match next(cur) {
       Some('"') => break,
-      Some('\\') => match cur.next() {
+      Some('\\') => match next(cur) {
         Some('n') => s.push('\n'),
         Some('t') => s.push('\t'),
         Some('r') => s.push('\r'),
@@ -52,6 +76,7 @@ fn parse_string(cur: &mut std::str::Chars<'_>) -> Result<Token, (String, Locatio
           return Err(("unexpected escape.".to_string(), get_position() + 1))
         }
       }
+      Some('\n') => return Err(("unexpected line feed.".to_string(), get_position() + 1)),
       Some(c) => s.push(c),
       None =>
         return Err(("expect ending of string.".to_string(), get_position() + 1))
@@ -59,7 +84,45 @@ fn parse_string(cur: &mut std::str::Chars<'_>) -> Result<Token, (String, Locatio
   }
 
   let len = s.len();
-  Ok(Token::LiteralValue { value: Literal::StrLit(s), loc: start_pos +  len})
+  Ok(Token::LiteralValue { value: Literal::StrLit(s), loc: start_pos + len})
+}
+
+fn parse_num(cur: &mut std::str::Chars<'_>, first: char) -> Result<Token, (String, Location)>{
+  let mut s = first.to_string();
+  let start_pos = get_position();
+
+  loop {
+    match next(cur) {
+      Some(c) if c.is_whitespace() || c.is_ascii_control() || (c.is_ascii_punctuation() && c != '.') => break,
+      Some(c) => s.push(c),
+      None => break
+    }
+  }
+
+  let len = s.len();
+  match s.parse::<i64>() {
+    Ok(num) => Ok(Token::LiteralValue { value: Literal::IntLit(num), loc: start_pos + len }),
+    Err(_) => match s.parse::<f64>() {
+      Ok(num) => Ok(Token::LiteralValue { value: Literal::NumLit(num), loc: start_pos + len }),
+      Err(_) => Err(("wrong number format.".to_string(), start_pos + len))
+    }
+  }
+}
+
+fn parse_ident(cur: &mut std::str::Chars<'_>, first: char) -> Token {
+  let mut s = first.to_string();
+  let start_pos = get_position();
+
+  loop {
+    match next(cur) {
+      Some(c) if c.is_whitespace() || c.is_ascii_control() => break,
+      Some(c) => s.push(c),
+      None => break
+    }
+  }
+
+  let len = s.len();
+  Token::Identifier { name: s, loc: start_pos + len}
 }
 
 pub fn tokenize(code: String) -> TokenPack {
@@ -68,16 +131,20 @@ pub fn tokenize(code: String) -> TokenPack {
   let mut cur = code.chars();
 
   loop {
-    match cur.next() {
+    match next(&mut cur) {
       Some(c) => match c {
         ';' => swallow_line(&mut cur),
         '"' => match parse_string(&mut cur) {
           Ok(res) => pack.add(res),
           Err((msg, loc)) => pack.throw(msg, loc)
         }
-        c => {
-          print!("{:?}", c); continue
+        '(' | ')' | ',' | '[' | ']' | '{' | '}' => pack.add(Token::Punctuation { p: c, loc: get_position() + 1 }),
+        c if c.is_numeric() => match parse_num(&mut cur, c) {
+          Ok(res) => pack.add(res),
+          Err((msg, loc)) => pack.throw(msg, loc)
         }
+        c if c.is_whitespace() || c.is_ascii_control() => continue,
+        c => pack.add(parse_ident(&mut cur, c))
       }
       None => break
     }
