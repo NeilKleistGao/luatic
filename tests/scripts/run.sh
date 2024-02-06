83013958c862cdc2e5c73092ce31a3e856c72bea
NeilKleistGao <neilkleist@gmail.com>
2024-02-06 21:28:56 +0800

    Fix test script

--- tests/scripts/run.sh
+++ tests/scripts/run.sh
@@ -6,6 +6,9 @@ for filename in $files
 do
   if ([[ $filename == *".luac" ]]) then
     echo "executing" $filename "..."
-    $(lua "./tests/ltc/"$filename) >> "./tests/ltc/"${filename//luac/check}
+    $(lua "./tests/ltc/"$filename) >> "./tests/ltc/"${filename//luac/check} &> "./tests/ltc/"${filename//luac/check}
+    if ([[ $? -ne 0 ]]) then
+      exit -1
+    fi
   fi
 done
