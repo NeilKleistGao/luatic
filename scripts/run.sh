6f41b169d785c245af37494bd74d7f78f79e17c4
NeilKleistGao <neilkleist@gmail.com>
2024-05-23 22:55:33 +0800

    WIP: Fix test script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ for filename in $files
 do
   if ([[ $filename == *".lynx" ]]) then
     echo "executing" $filename "..." # check compatibility
-    $(lua "./tests/ltc/"$filename) &> "./tests/ltc/"${filename//luac/check}
+    $(lua "./tests/ltc/"$filename)
     if ([[ $? -ne 0 ]]) then
       exit -1
     fi
